da71c5b15fdf731d4cc661a2c361dae417ae658c
Joao Lopes <joaorfclopes@gmail.com>
2022-02-09 13:07:23 +0000

    user routing and controllers done

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -7,13 +7,80 @@ export const createUser = async (req, res) => {
     password: hashSync(req.body.password)
   })
   try {
-    const createdUser = await user.save()
-    res.send({
-      _id: createdUser._id,
-      username: createdUser.username,
-      token: generateToken(createdUser)
-    })
+    const existentUser = await User.findOne({ username: user.username })
+    if (!existentUser) {
+      const createdUser = await user.save()
+      res.send({
+        message: 'user created successfully!',
+        user: {
+          _id: createdUser._id,
+          username: createdUser.username,
+          token: generateToken(createdUser)
+        }
+      })
+    } else {
+      res.status(404).send({ message: 'user already exists...' })
+    }
   } catch (error) {
-    res.status(401).send({ message: 'username already exists...' })
+    res.status(500).send({ message: 'error creating user', error })
+  }
+}
+
+export const findUser = async (req, res) => {
+  const user = await User.findOne({ username: req.query.user })
+  try {
+    if (user) {
+      res.send({ user })
+    } else {
+      res.status(404).send({ message: 'user not found...' })
+    }
+  } catch (error) {
+    res.status(500).send({ message: 'error finding user', error })
+  }
+}
+
+export const findAll = async (req, res) => {
+  const users = await User.find({})
+  try {
+    if (users.length > 0) {
+      res.send({ users })
+    } else {
+      res
+        .status(404)
+        .send({ message: 'there are no users in the database yet...' })
+    }
+  } catch (error) {
+    res.status(500).send({ message: 'error finding users', error })
+  }
+}
+
+export const updateUser = async (req, res) => {
+  const user = await User.findOne({ username: req.query.user })
+  try {
+    const { username, password } = req.body
+    if (user) {
+      if (username) user.username = username
+      if (password) user.password = hashSync(password)
+      const updatedUser = await user.save()
+      res.send({ message: 'user updated successfully!', user: updatedUser })
+    } else {
+      res.status(404).send({ message: 'user not found...' })
+    }
+  } catch (error) {
+    res.status(500).send({ message: 'error updating user', error })
+  }
+}
+
+export const deleteUser = async (req, res) => {
+  const user = await User.findOne({ username: req.query.user })
+  try {
+    if (user) {
+      const deleteUser = await user.remove()
+      res.send({ message: 'user deleted successfully!', user: deleteUser })
+    } else {
+      res.status(404).send({ message: 'user does not exist...' })
+    }
+  } catch (error) {
+    res.status(500).send({ message: 'error deleting user', error })
   }
 }
--- api/routes/userRoutes.js
+++ api/routes/userRoutes.js
@@ -1,8 +1,19 @@
-import { createUser } from '../controllers/userController.js'
+import {
+  createUser,
+  deleteUser,
+  findAll,
+  findUser,
+  updateUser
+} from '../controllers/userController.js'
 import { Router } from '../helpers/routesHelper.js'
 
 const userRoute = Router()
 
-userRoute.post('/create', createUser)
+userRoute
+  .post('/create', createUser)
+  .get('/find', findUser)
+  .get('/findAll', findAll)
+  .put('/update', updateUser)
+  .delete('/delete', deleteUser)
 
 export default userRoute
