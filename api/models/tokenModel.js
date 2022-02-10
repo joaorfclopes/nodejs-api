b7085f97c61908d604f6ff8c25a370219dd20309
Joao Lopes <joaorfclopes@gmail.com>
2022-02-10 12:19:59 +0000

    timestamps added to models

--- api/models/tokenModel.js
+++ api/models/tokenModel.js
@@ -1,10 +1,13 @@
 import { Model, Schema } from '../helpers/modelsHelper.js'
 
-const tokenSchema = new Schema({
-  user: { type: String, required: true },
-  value: { type: String, required: true, unique: true },
-  expired: { type: Boolean, required: true, default: false }
-})
+const tokenSchema = new Schema(
+  {
+    user: { type: String, required: true },
+    value: { type: String, required: true, unique: true },
+    expired: { type: Boolean, required: true, default: false }
+  },
+  { timestamps: true }
+)
 
 const Token = Model('Token', tokenSchema)
 export default Token
--- api/models/userModel.js
+++ api/models/userModel.js
@@ -1,9 +1,12 @@
 import { Model, Schema } from '../helpers/modelsHelper.js'
 
-const userSchema = new Schema({
-  username: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
-})
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+  },
+  { timestamps: true }
+)
 
 const User = Model('User', userSchema)
 export default User
