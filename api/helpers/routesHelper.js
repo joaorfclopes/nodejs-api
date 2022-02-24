5b6eb2dad6d82abdd638190e9437e2761bf63296
Joao Lopes <joaorfclopes@gmail.com>
2022-02-24 15:26:25 +0000

    jwtSecret added to isAuth

--- api/helpers/routesHelper.js
+++ api/helpers/routesHelper.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
+import { jwtSecret } from '../../globals.js'
 import Token from '../models/tokenModel.js'
 
 export const Router = express.Router
@@ -8,7 +9,7 @@ export const isAuth = (req, res, next) => {
   const authorization = req.headers.authorization
   if (authorization) {
     const token = authorization.slice(7, authorization.length)
-    jwt.verify(token, 'somethingsecret', async (err, decode) => {
+    jwt.verify(token, jwtSecret, async (err, decode) => {
       if (err) {
         const existentToken = await Token.findOne({ value: token })
         if (existentToken) {
